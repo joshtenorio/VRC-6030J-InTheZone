a965f3aa26dbd818b7d2a6afa025855387ffbec3
probablyn0t <joshliam09@gmail.com>
2018-02-05 20:30:05 -0800

    Add files via upload

--- code/src/opcontrolActions.c
+++ code/src/opcontrolActions.c
@@ -67,33 +67,33 @@ void opcontrolChainBar(){
    int current = encoderGet(encoderChainB);
    static int cbTarget = 0; 
 
-   if(joystickGetDigital(2, 5, JOY_UP)){
+   if(joystickGetDigital(2, 5, JOY_UP)){ //move chainbar
      speed = 75; 
-	 print("button press");
+	 print("button press");  
 	 cbTarget = encoderGet(encoderChainB);
    }
-   else if(joystickGetDigital(2, 5, JOY_DOWN)){
+   else if(joystickGetDigital(2, 5, JOY_DOWN)){ //move chainbar
      speed = -75;
 	 print("button press");
 	 cbTarget = encoderGet(encoderChainB);
    } 
    
    else if(joystickGetDigital(2, 8, JOY_LEFT)){                                           
-     speed = -min(60, max(-60, PID(current, -60, 1, 0.8, 0, 0)));    //holds chainbar right above mobile goal lifter
+     speed = -min(60, max(-60, PID(current, -60, 1, 0.8, 0, 0)));    //holds chainbar right above mobile goal lifter (preset)
    }  
-   else {
-	   if (current > -20) { //checks if chainbar is resting above mobile goal, if it is will not run PID
+   else { //if no chainbar control buttons are being pressed
+	   if (current > -15) { //checks if chainbar is resting above mobile goal, if it is will not run PID (dead band)
 		   speed=0;
 	   }
 	   else {
-		   speed = -min(60, max(-60, PID(current, cbTarget, 1, 0.9, 0, 0))); //running PID
+		   speed = -min(60, max(-60, PID(current, cbTarget, 1, 0.9, 0, 0))); //run hold last position PID
 		   printf("Chainbar value, target, speed: %d\n, %d, %d\n", encoderGet(encoderChainB), cbTarget, smartMotorGet(MOTORS_CHAINB));
 	   }
    } 
-   chainBar(speed);
+   chainBar(speed); //setting speed for chainbar
  }
 
-static float linearGearStack[3] = {0, 0, 0}; //these two arrays need to be updated with real target values
+//static float linearGearStack[3] = {0, 0, 0}; //these two arrays need to be updated with real target values
 static float chainBarStack[3] = {0, -25, -36}; //each float in the array corresponds to the number of cones in stack
 void opcontrolStack(){
 	static int coneCount = 0;  //each float in the array is a target value for each mechanism
@@ -101,10 +101,10 @@ void opcontrolStack(){
 	static int prevButtonState = 0; //holds last state of down button vexnet joystick, group 7
 
 	int chainbarSpeed;
-	int linearGearSpeed;
+	//int linearGearSpeed;
 	int coneGrabberSpeed;
 	int cbCurrent = encoderGet(encoderChainB);
-	int lgCurrent = encoderGet(shaftLinearGear);
+	//int lgCurrent = encoderGet(shaftLinearGear);
 	if(joystickGetDigital(2, 7, JOY_RIGHT)){ //reset cone stack
 		coneCount = 0;
 	}
@@ -115,24 +115,24 @@ void opcontrolStack(){
 			chainbarSpeed = -min(60, max(-60, PID(cbCurrent, -180, 1, 0.8, 0, 0)));
 			delay(1);
 		}
-
+		/*
 		linearGearSpeed = -min(120, max(-120, PID(lgCurrent, linearGearStack[coneCount], 2, 0.8, 0, 0)));
 		while ((abs(smartMotorGet(MOTORS_LINEAR)) > 20)) {
 			linearGearSpeed = -min(120, max(-120, PID(lgCurrent, linearGearStack[coneCount], 2, 0.8, 0, 0))); //bring linear gear to driver station level
 			delay(1);
-		}
+		} */
 	
 		coneGrabberSpeed = 0;
 		delay(20);
 		coneGrabberSpeed = -90; //this needs speed to pick up cone
 		delay(200);
 		coneGrabberSpeed = 0;
-
+		/*
 		linearGearSpeed = -min(127, max(-127, PID(lgCurrent, linearGearStack[coneCount], 2, 0.8, 0, 0)));
 		while ((abs(smartMotorGet(MOTORS_LINEAR)) > 20)) {
 			linearGearSpeed = -min(127, max(-127, PID(lgCurrent, linearGearStack[coneCount], 2, 0.7, 0, 0))); //bring linear gear to stack
 			delay(1);
-		}
+		} */
 
 		chainbarSpeed = -min(60, max(-60, PID(cbCurrent, chainBarStack[coneCount], 1, 0.8, 0, 0)));
 		while (abs(smartMotorGet(MOTORS_CHAINB)) > 20) { //bring chainbar to mobile goal stack
@@ -143,7 +143,7 @@ void opcontrolStack(){
 		coneCount += 1;
 	}
 
-	linearGear(linearGearSpeed);
+	//linearGear(linearGearSpeed);
 	chainBar(chainbarSpeed);
 	coneGrabber(coneGrabberSpeed);
 }
@@ -155,6 +155,11 @@ void opcontrolDebug() {
 		encoderReset(leftDriveShaft);
 		encoderReset(rightDriveShaft);
 		encoderReset(shaftLinearGear);
+		tankDrive(0, 0);
+		mobileGoal(0);
+		linearGear(0);
+		chainBar(0);
+		coneGrabber(0);
 	}
 
 }
